3b2c6193438294255a4b6a9c8e5e23afc3a5f934
Katie Sidebotham <katiesidebotham54@gmail.com>
2022-02-02 20:38:07 -0500

    Edited Appointment Class

--- src/Appointment.java
+++ src/Appointment.java
@@ -3,13 +3,22 @@ public class Appointment {
     private Timeslot slot;
     private Location location;
 
+    public Appointment(Patient patient, Timeslot slot, Location location){
+        this.patient = patient;
+        this.slot = slot;
+        this.location = location;
+    }
+
     @Override
     public boolean equals(Object obj) {
-
+        if(obj == this){
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-
+        return patient.fname + " " + patient.lname + ", DOB: " + patient.dob + " , Appointment detail: " + slot.date + ", " + slot.time + ", ";
     }
 }
