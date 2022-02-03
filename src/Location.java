e066d2e72101d4eb032543714c2429fe3549aa86
kevinarbito <arbito.d.kevin@gmail.com>
2022-02-02 23:23:59 -0500

    Location class defined (City, Zip code, County) in String format

--- src/Location.java
+++ src/Location.java
@@ -1,3 +1,17 @@
 public enum Location {
-        Bridgewater, Piscataway, Princeton, Morristown, Union;
+    BRIDGEWATER("Bridgewater", "08807", "Somerset County"),
+    PISCATAWAY("Piscataway", "08854", "Middlesex County"),
+    PRINCETON("Princeton", "08542", "Mercer County"),
+    MORRISTOWN("Morristown", "07960", "Morris County"),
+    UNION("Union", "07083", "Union County");
+
+    private final String city;
+    private final String zipCode;
+    private final String county;
+
+    Location(String city, String zipCode, String county) {
+        this.city = city;
+        this.zipCode = zipCode;
+        this.county = county;
     }
+}
