4ddf926a23e155d26da5920ea4c323bd16d7a143
Katie Sidebotham <katiesidebotham54@gmail.com>
2022-02-04 01:07:18 -0500

    Working on date class

--- src/Date.java
+++ src/Date.java
@@ -1,3 +1,5 @@
+import java.util.Calendar;
+
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
@@ -9,13 +11,28 @@ public class Date implements Comparable<Date> {
         this.day = day;
     }
     public Date(String date) {
-        Object newDate = date;
+        date.useDelimiter("/");
+
+
+        Date date = new Date();
     } //take “mm/dd/yyyy” and create a Date object
     public Date() {
-
+        Calendar c = Calendar.getInstance();
+        Date today = new Date(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DATE));
     } //create an object with today’s date (see Calendar class)
     public boolean isValid() {
+        //must be valid calendar date
 
+        //check if date of birth is today or future date
+        if(this >= today){
+            return false;
+        }
+        if(this <= today || this > )
+
+
+        //date of birth is not today or future date
+        //date cannot be today, day before today, date beyond one year
+        //
     }
     @Override
     public int compareTo(Date date) {
