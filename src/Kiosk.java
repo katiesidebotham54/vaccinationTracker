9e6a7dc017ed8ae4a7a7962da58c0c703e857ee5
Katie Sidebotham <42896384+katiesidebotham54@users.noreply.github.com>
2022-02-03 22:04:15 -0500

    Started Kiosk Class

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,6 +1,27 @@
 public class Kiosk {
     public void run(){
         Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            command = sc.next();
+            if( command = "B"){
+
+            } else if( command == "C"){
+
+            } else if(command == "CP"){
+
+            } else if(command == "P"){
+
+            } else if(command == "PZ"){
+
+            } else if(command == "PP"){
+
+            } else if(command == "Q"){
+                sc.close();
+                System.out.println("Kiosk session ended.");
+            } else{
+                // invalid input
+            }
+        }
 
     }
 }
