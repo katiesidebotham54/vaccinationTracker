6e6b23fbc247167bc928e784ce322e186f261801
kevinarbito <arbito.d.kevin@gmail.com>
2022-02-03 22:40:51 -0500

    Updated Date & Time

--- src/Date.java
+++ src/Date.java
@@ -3,6 +3,11 @@ public class Date implements Comparable<Date> {
     private int month;
     private int day;
 
+    Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
     public Date(String date) {
 
     } //take “mm/dd/yyyy” and create a Date object
