05866fa48a872d7ad070c15bbb88f1e21dbc3cbb
Katie Sidebotham <42896384+katiesidebotham54@users.noreply.github.com>
2022-02-08 22:40:21 -0500

    Working on Schedule Class

--- src/Schedule.java
+++ src/Schedule.java
@@ -4,11 +4,8 @@ authors: @katiesidebotham @kevinarbito
  */
 public class Schedule {
 
-    Schedule(Patient patient, Date date, Time time, Location location){
-        this.patient = patient;
-        this.date = date;
-        this.time = time;
-        this.location = location;
+    Schedule(){
+
     }
 
     private Appointment [] appointments;
@@ -18,25 +15,45 @@ public class Schedule {
 
     } //return the index, or NOT_FOUND
     private void grow() {
-
+        Appointment[] temp = new Appointment[numAppts + 4];
+        for (int i = 0; i < numAppts; i++){
+            temp[i] = appointments[i];
+        }
+        appointments = temp;
     } //increase the capacity of the container by 4
     public boolean add(Appointment appt) {
+        if(find(appt) == NOT_FOUND){
+            return false;
+        } else{
+            //check if same patient doing multiple appointments, make sure appointments aren't on same day
+            //if patient = appt.patient
+            //
+        }
         if(numAppts == appointments.length){
             grow();
         }
-        //if same patient doing multiple appointments, make sure appointments aren't on same day
-
+        appointments[numAppts] = appt;
         numAppts++;
     }
+
     public boolean remove(Appointment appt) {
 
     }
     public void print() {
-
+        for(Appointment a: appointments){
+            System.out.println(a.toString());
+        }
     } //print all the appointments in current order
+    public void sortByZip(){
+
+    }
     public void printByZip() {
 
     } //sort by zip codes and print
+
+    public void sortByPatient(){
+
+    }
     public void printByPatient() {
 
     } //sort by patient and print }
