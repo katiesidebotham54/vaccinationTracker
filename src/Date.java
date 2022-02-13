57db13922428e975b5f5ae76c828237cda086560
kevinarbito <arbito.d.kevin@gmail.com>
2022-02-13 17:07:48 -0500

    javadoc in the works

--- src/Date.java
+++ src/Date.java
@@ -24,6 +24,11 @@ public class Date implements Comparable<Date> {
         this.day = day;
     }
 
+    /**
+     * This constructor takes in string format of date and creates the Date object using integers.
+     * This allows for easy acces within the date object.
+     * @param date date needed to create object
+     */
     public Date(String date) {
         String[] parts = date.split("/");
         this.month = Integer.parseInt(parts[0]);
@@ -31,6 +36,10 @@ public class Date implements Comparable<Date> {
         this.year = Integer.parseInt(parts[2]);
     } //take “mm/dd/yyyy” and create a Date object
 
+    /**
+     * This constructor takes in no parameters, it simply gets today's date using calendar class and stores
+     * today's date within the object.
+     */
     public Date() {
         Calendar c = Calendar.getInstance();
         this.month = c.get(Calendar.MONTH);
@@ -38,6 +47,12 @@ public class Date implements Comparable<Date> {
         this.year = c.get(Calendar.YEAR);
     } //create an object with today’s date (see Calendar class)
 
+    /**
+     * isLeapYear() checks whether the year within the date is a leap year. It goes through checks by modding
+     * QUADRENNIAL, CENTENNIAL, and QUATERCENTENNIAL. It returns its boolean value true or false based on its
+     * results from each conditional.
+     * @return true if leap year, false otherwise.
+     */
     public boolean isLeapYear(){
             if(this.year % QUADRENNIAL == 0){
                 if(this.year % CENTENNIAL == 0) {
@@ -49,20 +64,26 @@ public class Date implements Comparable<Date> {
             return false;
     }
 
+    // Getter for year
     public int getYear(){
         return this.year;
     }
 
+    // Getter for day
     public int getDay() {
         return this.day;
     }
 
+    // Getter for month
     public int getMonth() {return this.month;}
 
-    public Date getToday(){
-        return new Date();
-    }
 
+    /**
+     * checkDay() checks whether the date that was given is actually a valid date based on the month, days, and year.
+     * It checks month for its max ammount of days. It also takes whether the year is a leap year. If the date is valid,
+     * and within range, it will return true, false otherwise.
+     * @return true if date is within the correct range, false otherwise
+     */
     public boolean checkDays(){
         switch(this.month){
             case 1, 3, 5, 7, 8, 10, 12 -> {
@@ -85,12 +106,21 @@ public class Date implements Comparable<Date> {
         return false;
     }
 
+    /**
+     * This method overrides and returns the date in string format.
+     * @return the date in string format.
+     */
     @Override
     public String toString() {
         return this.month + "/" + this.day + "/" + this.year;
     }
 
-
+    /**
+     * isValid() checks to see if the date that was input is a valid date with format MM/DD/YYYY. It also checks to see
+     * whether the date has a valid number in terms of month, days, and year. It returns false if they are not within
+     * range provided, true otherwise.
+     * @return Boolean: true if the months, days, and year is within the range, false otherwise.
+     */
     public boolean isValid() {
         //check if valid input (mm/dd/yyyy)
         if(this.toString().matches("\\d{1}/\\d{1}/\\d{4}") || this.toString().matches("\\d{2}/\\d{2}/\\d{4}") || this.toString().matches("\\d{1}/\\d{2}/\\d{4}") || this.toString().matches("\\d{2}/\\d{1}/\\d{4}") ) { // has to check for m/m/yyyy because it is // in integer form
@@ -117,6 +147,12 @@ public class Date implements Comparable<Date> {
         return true;
     }
 
+    /**
+     * This method overrides compareTo and checks whether the dates are equal to, year is greater or less than, month
+     * is greater than or less than, and days is greater than or less than.
+     * @param date date to be compared to
+     * @return -1 if less than, 1 if greater than, 0 if equal to.
+     */
     @Override
     public int compareTo(Date date) {
         if(this.equals(date)){
